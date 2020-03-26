4555522d43d69e65bdc4081dc93337fa8a254301
The Dog Rex <vyalikov@mail.ru>
2020-03-26 18:07:27 +0300

    charset fix

--- config.php
+++ config.php
@@ -11,7 +11,9 @@ try
 	// Соединяемся с БД
 	$dbPDO = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS); 
 	$dbPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$dbh->exec("set names utf8");
 }
+
 catch(PDOException $e)
 {
 	echo $e->getMessage();
--- database_dump/test.sql
+++ database_dump/test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 26 2020 г., 14:09
+-- Время создания: Мар 26 2020 г., 15:05
 -- Версия сервера: 10.4.10-MariaDB
 -- Версия PHP: 7.3.12
 
@@ -33,18 +33,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` tinytext NOT NULL,
   `role_id` int(11) NOT NULL,
-  UNIQUE KEY `username` (`username`) USING HASH,
+  UNIQUE KEY `username` (`username`(20)) USING HASH,
   KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `role_id`) VALUES
-(1, 'ÐÐ½Ñ‚Ð¾Ð½', 1),
-(2, 'James Bond 007', 3),
-(3, 'Ð¡ÑƒÐ¿ÐµÑ€Ð¼ÐµÐ½', 2);
+(1, 'Чак Норрис', 1),
+(2, 'James Bond 007', 2),
+(3, 'Вася Пупкин', 3),
+(4, 'Guest 23891', 4);
 
 -- --------------------------------------------------------
 
@@ -56,7 +57,7 @@ DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE IF NOT EXISTS `user_role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `rolename` tinytext NOT NULL,
-  UNIQUE KEY `rolename` (`rolename`) USING HASH,
+  UNIQUE KEY `rolename` (`rolename`(20)) USING HASH,
   KEY `id` (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
@@ -65,10 +66,10 @@ CREATE TABLE IF NOT EXISTS `user_role` (
 --
 
 INSERT INTO `user_role` (`id`, `rolename`) VALUES
-(1, 'Ð ÐµÐ´Ð°ÐºÑ‚Ð¾Ñ€'),
-(2, 'ÐœÐ¾Ð´ÐµÑ€Ð°Ñ‚Ð¾Ñ€'),
-(3, 'ÐÐ´Ð¼Ð¸Ð½Ð¸ÑÑ‚Ñ€Ð°Ñ‚Ð¾Ñ€'),
-(4, 'ÐžÐ±Ñ‹Ñ‡Ð½Ñ‹Ð¹ ÐŸÐ¾Ð»ÑŒÐ·Ð¾Ð²Ð°Ñ‚ÐµÐ»ÑŒ');
+(1, 'Администратор'),
+(2, 'Модератор'),
+(3, 'Пользователь'),
+(4, 'Гость');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
